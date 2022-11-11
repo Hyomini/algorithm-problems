df1c9f6e2db0c6d7cdc00ceefec84860177202ef
Hyomini <dksgyals1@gmail.com>
2022-11-11 17:51:43 +0900

    벽돌깨기 수정

--- 벽돌깨기/break_brick.py
+++ 벽돌깨기/break_brick.py
@@ -3,35 +3,39 @@ import itertools
 import copy
 from collections import deque
 
+# 로컬에서 input파일로 테스트가 필요할 때 밑의 주석을 제거하고 테스트
 sys.stdin = open("input.txt", "r")
 delta = [(0,-1), (0,1), (1,0), (-1,0)]
 
 def get_permutations(N, W):
-    return itertools.permutations(range(W), N)
+    return itertools.product(range(W), repeat=N)
 
 def bomb(board, y_order, W, H):
-    brick_num = 0
+    brick_count = 0
     for posy in y_order:
         q = deque()
         for posx in range(H):
             if board[posx][posy] > 0:
+                q.append((posx, posy, board[posx][posy]))
                 board[posx][posy] = 0
-                q.append((posx, posy))
                 break
         while q:
-            cur_x, cur_y = q.popleft()
+            cur_x, cur_y, brick_num = q.popleft()
             for i in range(len(delta)):
-                for num in range(1, board[cur_x][cur_y]):
-                    if board[cur_x + num * delta[i][0]][cur_y + num * delta[i][1]] > 1:
-                        q.append((cur_x + num * delta[i][0], cur_y + num * delta[i][1]))
-                    board[cur_x + num * delta[i][0]][cur_y + num * delta[i][1]] = 0
+                for num in range(1, brick_num):
+                    if cur_x + num * delta[i][0] >= 0  and cur_x + num * delta[i][0] < H and \
+                        cur_y + num * delta[i][1] >= 0  and cur_y + num * delta[i][1] < W:
+                        if board[cur_x + num * delta[i][0]][cur_y + num * delta[i][1]] > 1:
+                            q.append((cur_x + num * delta[i][0], cur_y + num * delta[i][1], board[cur_x + num * delta[i][0]][cur_y + num * delta[i][1]]))
+                        board[cur_x + num * delta[i][0]][cur_y + num * delta[i][1]] = 0
+        
         sort_board(board, W, H)
 
     for i in range(H):
         for j in range(W):
             if board[i][j] > 0:
-                brick_num += 1
-    return brick_num
+                brick_count += 1
+    return brick_count
 
 def sort_board(board, W, H):
     for j in range(W):
@@ -50,11 +54,11 @@ for test_case in range(1, T + 1):
     N, W, H = map(int, sys.stdin.readline().split())
     board = [list(map(int, sys.stdin.readline().split())) for _ in range(H)]
 
-    brick_num = 0
+    brick_count = 0
     for i in range(H):
         for j in range(W):
             if board[i][j] > 0:
-                brick_num += 1
+                brick_count += 1
 
     y_orders = get_permutations(N, W)
     min_broken_brick_num = 181
