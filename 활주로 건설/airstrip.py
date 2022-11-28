9e696ee68b836a317fb7079a5cc4e94e0e2e38c6
DESKTOP-MIFR9EP\hyomin <dksgyals1@gmail.com>
2022-11-29 00:38:27 +0900

    활주로 건설 답은 맞춘 코드 업로드

--- 활주로 건설/airstrip.py
+++ 활주로 건설/airstrip.py
@@ -1,10 +1,35 @@
-import sys
-sys.stdin = open("input.txt", "r")
+# import sys
+# sys.stdin = open("input.txt", "r")
 
 T = int(input())
 
 # 활주로 건설 가능성을 체크
 def is_possible(line):
+    if len(set(line)) == 1:
+        return True
+
+    check = False
+    cnt = 1
+    for i in range(1, N):
+        if line[i-1] == line[i]:
+            cnt += 1
+        else:
+            if check:
+                if cnt < X:
+                    return False
+                check = False
+                cnt -= X
+            if line[i-1] - line[i] == 1:
+                check = True
+                cnt = 1
+            elif line[i] - line[i-1] == 1: # line[i-1] < line[i]
+                if cnt < X:
+                    return False
+                cnt = 1
+            else:
+                return False
+    if check and cnt < X:
+        return False
     return True
 
 for test_case in range(1, T + 1):
