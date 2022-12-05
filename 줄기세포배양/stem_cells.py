64e8f3ee83aa53a4e16008fe2e5668654e669d4f
Hyomini <dksgyals1@gmail.com>
2022-12-05 17:25:09 +0900

    줄기세포배양 수정

--- 줄기세포배양/stem_cells.py
+++ 줄기세포배양/stem_cells.py
@@ -17,29 +17,47 @@ def init_grid():
 
 # key: position, value[0]: 생명력, value[1]: 수명
 def update_grid():
-    # 번식(inactive cells에서 수명이 0이면 사방에 번식, max값으로 갱신)하고 inactive cells에 추가
-    # 수명이 0인 cell을 active cell에 추가
-    for cell_pos in inactive_cells.keys():
-        if inactive_cells[cell_pos][1] == 0:
-            reproduce(cell_pos)
-            active_cells[cell_pos] = inactive_cells[cellpos][:]
-    # inactive cells 업데이트
-    # 1. 수명이 0인 cell을 remove
-    # 2. 수명이 0보다 큰 cell의 수명을 1씩 감소시킨다
-    for cell_pos in inactive_cells.keys():
-        if inactive_cells
-
     # active cells 업데이트
     # 1. 수명이 0보다 큰 cell의 수명을 1씩 감소시킨다
     # 2. 수명이 0인 cell을 remove
-    for cell_pos in active_cells.keys():
+    active_keys = list(active_cells.keys())
+    for cell_pos in active_keys:
+        if active_cells[cell_pos][1] > 0:
+            active_cells[cell_pos][1] -= 1
+    for cell_pos in active_keys:
+        if active_cells[cell_pos][1] == 0:
+            active_cells.pop(cell_pos)
+            grid[cell_pos[0]][cell_pos[1]] = -1
+
+    # 1. 번식(inactive cells에서 수명이 0이면 사방에 번식, max값으로 갱신)
+    # 1-2. 수명이 0인 cell을 inactive cells에서 remove
+    # 1-3. 수명이 0보다 큰 cell의 수명을 1씩 감소시킨다
+    inactive_keys = list(inactive_cells.keys())
+    reproduce_targets = list()
+    for cell_pos in inactive_keys:
+        if inactive_cells[cell_pos][1] == 0:
+            reproduce_targets.append(cell_pos)
+    for cell_pos in inactive_keys:
+        if inactive_cells[cell_pos][1] > 0:
+            inactive_cells[cell_pos][1] -= 1
+    # reproduce
+    for target in reproduce_targets:
+        reproduce(target)
+        inactive_cells.pop(target)
+
+    # 수명이 0인 cell들 inactive cells에 추가
+    inactive_keys = list(inactive_cells.keys())
+    for cell_pos in inactive_keys:
+        if inactive_cells[cell_pos][1] == 0:
+            active_cells[cell_pos] = [inactive_cells[cell_pos][0], inactive_cells[cell_pos][0]]
 
 
 def reproduce(cell_pos):
     for i in range(len(delta)):
         new_cellx, new_celly = cell_pos[0] + delta[i][0], cell_pos[1] + delta[i][1]
-        if new_cellx >= 0 and new_cellx < len(grid) and new_celly >= 0 and new_celly < len(grid[0]):
+        if new_cellx >= 0 and new_cellx < len(grid) and new_celly >= 0 and new_celly < len(grid[0]) and grid[new_cellx][new_celly] > -1:
             grid[new_cellx][new_celly] = max(grid[new_cellx][new_celly], inactive_cells[cell_pos][0])
+            inactive_cells[(new_cellx, new_celly)] = [grid[new_cellx][new_celly], grid[new_cellx][new_celly]]
 
 
 T = int(input())
@@ -55,9 +73,10 @@ for test_case in range(1, T + 1):
             if grid[i][j] > 0:
                 inactive_cells[(i,j)] = [grid[i][j], grid[i][j]]
     
-    print(inactive_cells)
-    # for i in range(K):
-    #     update_grid()
+    for i in range(1, K+1):
+        update_grid()
+        print(i, inactive_cells)
+        print(active_cells)
     
     cnt = len(active_cells) + len(inactive_cells)
 
